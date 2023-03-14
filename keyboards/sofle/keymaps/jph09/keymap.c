655a08993c5e27b1e97daf403348f2e33cb7a338
Jonathan Hartshorn <j.hartshorn@outlook.com>
2023-03-14 11:52:24 +0000

    bug fix + minor mods

--- keyboards/sofle/keymaps/jph09/keymap.c
+++ keyboards/sofle/keymaps/jph09/keymap.c
@@ -191,24 +191,24 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 [_NAV] = LAYOUT( \
   XXXXXXX, XXXXXXX,     XXXXXXX,    XXXXXXX,       XXXXXXX,          XXXXXXX,                     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,  XXXXXXX, \
-  XXXXXXX, TD(TD_BOOT), TD(TD_TAP), TD(TD_QWERTY), TD(TD_COLEMAKDH), CG_TOGG,                     KC_AGIN, S(KC_INS), C(KC_INS), S(KC_INS),  KC_UNDO,  XXXXXXX, \
-  XXXXXXX, KC_LGUI,     KC_LALT,    KC_LCTL,       KC_LSFT,          XXXXXXX,                     CW_TOGG, KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT, KC_CAPS, \
-  XXXXXXX, MO(_BUTTON), XXXXXXX,    TD(TD_NUM),    TD(TD_NAV),       XXXXXXX, KC_MUTE,   XXXXXXX, KC_INS,  KC_HOME, KC_PGDN, KC_PGUP, KC_END,   XXXXXXX, \
+  XXXXXXX, TD(TD_BOOT), TD(TD_TAP), TD(TD_QWERTY), TD(TD_COLEMAKDH), CG_TOGG,                     KC_INS,  KC_HOME, KC_PGDN, KC_PGUP, KC_END,   XXXXXXX, \
+  XXXXXXX, KC_LGUI,     KC_LALT,    KC_LCTL,       KC_LSFT,          KC_CAPS,                     KC_CAPS, KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT, CW_TOGG, \
+  XXXXXXX, MO(_BUTTON), XXXXXXX,    TD(TD_NUM),    TD(TD_NAV),       XXXXXXX, KC_MUTE,   XXXXXXX, KC_AGIN, S(KC_INS), C(KC_INS), S(KC_DEL),  KC_UNDO, XXXXXXX, \
                                       XXXXXXX, XXXXXXX, _______, _______, _______,          KC_ENT, KC_BSPC, KC_DEL, XXXXXXX, XXXXXXX \
 ),
 
 [_MOUSE] = LAYOUT( \
   XXXXXXX, XXXXXXX,     XXXXXXX,    XXXXXXX,       XXXXXXX,          XXXXXXX,                     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, \
-  XXXXXXX, TD(TD_BOOT), TD(TD_TAP), TD(TD_QWERTY), TD(TD_COLEMAKDH), CG_TOGG,                     KC_AGIN, S(KC_INS), C(KC_INS), S(KC_INS),  KC_UNDO, XXXXXXX, \
-  XXXXXXX, KC_LGUI,     KC_LALT,    KC_LCTL,       KC_LSFT,          XXXXXXX,                     XXXXXXX, KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R, XXXXXXX, \
-  XXXXXXX, MO(_BUTTON), XXXXXXX,    TD(TD_SYM),    TD(TD_MOUSE),     XXXXXXX, KC_MUTE,   XXXXXXX, XXXXXXX, KC_WH_L, KC_WH_D, KC_WH_U, KC_WH_R, XXXXXXX, \
-                                      XXXXXXX, XXXXXXX, _______, _______, _______,          KC_BTN2, KC_BTN1, KC_BTN3, XXXXXXX, XXXXXXX \
+  XXXXXXX, TD(TD_BOOT), TD(TD_TAP), TD(TD_QWERTY), TD(TD_COLEMAKDH), CG_TOGG,                     XXXXXXX, KC_WH_L, KC_WH_D, KC_WH_U, KC_WH_R, XXXXXXX, \
+  XXXXXXX, KC_LGUI,     KC_LALT,    KC_LCTL,       KC_LSFT,          KC_CAPS,                     XXXXXXX, KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R, XXXXXXX, \
+  XXXXXXX, MO(_BUTTON), XXXXXXX,    TD(TD_SYM),    TD(TD_MOUSE),     XXXXXXX, KC_MUTE,   XXXXXXX, KC_AGIN, S(KC_INS), C(KC_INS), S(KC_DEL),  KC_UNDO, XXXXXXX, \
+                                      XXXXXXX, XXXXXXX, KC_BTN3, KC_BTN1, KC_BTN2,          KC_BTN2, KC_BTN1, KC_BTN3, XXXXXXX, XXXXXXX \
 ),
 
 [_MEDIA] = LAYOUT( \
   XXXXXXX, XXXXXXX,     XXXXXXX,    XXXXXXX,       XXXXXXX,          XXXXXXX,                     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, \
   XXXXXXX, TD(TD_BOOT), TD(TD_TAP), TD(TD_QWERTY), TD(TD_COLEMAKDH), CG_TOGG,                     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, \
-  XXXXXXX, KC_LGUI,     KC_LALT,    KC_LCTL,       KC_LSFT,          XXXXXXX,                     KC_MPRV, KC_VOLD, KC_VOLU, KC_MNXT, XXXXXXX, XXXXXXX, \
+  XXXXXXX, KC_LGUI,     KC_LALT,    KC_LCTL,       KC_LSFT,          KC_CAPS,                     KC_MPRV, KC_VOLD, KC_VOLU, KC_MNXT, XXXXXXX, XXXXXXX, \
   XXXXXXX, MO(_BUTTON), XXXXXXX,    TD(TD_FUN),    TD(TD_MEDIA),     XXXXXXX, KC_MUTE,   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, \
                                       XXXXXXX, XXXXXXX, _______, _______, _______,          KC_MSTP, KC_MPLY, KC_MUTE, XXXXXXX, XXXXXXX \
 ),
@@ -216,15 +216,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_NUM] = LAYOUT( \
   KC_GRV,  KC_1,  KC_2,  KC_3,  KC_4, KC_5,                        KC_6,    KC_7,             KC_8,          KC_9,       KC_0,        KC_MINS, \
   KC_GRV,  KC_LBRC, KC_7, KC_8, KC_9, KC_RBRC,                     CG_TOGG, TD(TD_COLEMAKDH), TD(TD_QWERTY), TD(TD_TAP), TD(TD_BOOT), XXXXXXX, \
-  KC_GRV,  KC_SCLN, KC_4, KC_5, KC_6, KC_EQL,                      XXXXXXX, KC_RSFT,          KC_RCTL,       KC_LALT,    KC_RGUI,     XXXXXXX, \
+  KC_GRV,  KC_SCLN, KC_4, KC_5, KC_6, KC_EQL,                      KC_CAPS, KC_RSFT,          KC_RCTL,       KC_LALT,    KC_RGUI,     XXXXXXX, \
   KC_GRV,  KC_NUHS, KC_1, KC_2, KC_3, KC_NUBS, KC_MUTE,   XXXXXXX, XXXXXXX, TD(TD_NUM),       TD(TD_NAV),    KC_RALT,    MO(_BUTTON), XXXXXXX, \
-            XXXXXXX, KC_ENTER, KC_DOT, KC_0, KC_MINS,          _______, _______, _______, XXXXXXX, XXXXXXX \
+            KC_ENTER, KC_TAB, KC_DOT, KC_0, KC_MINS,          _______, _______, _______, XXXXXXX, XXXXXXX \
 ),
 
 [_SYM] = LAYOUT( \
   XXXXXXX,   XXXXXXX,    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                        XXXXXXX, XXXXXXX,          XXXXXXX,       XXXXXXX,    XXXXXXX,     XXXXXXX, \
   XXXXXXX,   S(KC_LBRC), S(KC_7), S(KC_8), S(KC_9), S(KC_RBRC),                     CG_TOGG, TD(TD_COLEMAKDH), TD(TD_QWERTY), TD(TD_TAP), TD(TD_BOOT), XXXXXXX, \
-  S(KC_GRV), S(KC_SCLN), S(KC_4), S(KC_5), S(KC_6), S(KC_EQL),                      XXXXXXX, KC_RSFT,          KC_RCTL,       KC_LALT,    KC_RGUI,     XXXXXXX, \
+  S(KC_GRV), S(KC_SCLN), S(KC_4), S(KC_5), S(KC_6), S(KC_EQL),                      KC_CAPS, KC_RSFT,          KC_RCTL,       KC_LALT,    KC_RGUI,     XXXXXXX, \
   XXXXXXX,   S(KC_NUHS), S(KC_1), S(KC_2), S(KC_3), S(KC_NUBS), KC_MUTE,   XXXXXXX, XXXXXXX, TD(TD_SYM),       TD(TD_MOUSE),  KC_RALT,  MO(_BUTTON),   XXXXXXX, \
                   XXXXXXX, XXXXXXX, S(KC_DOT), S(KC_0), S(KC_MINS),          _______, _______, _______, XXXXXXX, XXXXXXX \
 ),
@@ -232,16 +232,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_FUN] = LAYOUT( \
   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                     XXXXXXX, XXXXXXX,          XXXXXXX,       XXXXXXX,    XXXXXXX,     XXXXXXX, \
   XXXXXXX, KC_F12,  KC_F7,   KC_F8,   KC_F9,   KC_PSCR,                     CG_TOGG, TD(TD_COLEMAKDH), TD(TD_QWERTY), TD(TD_TAP), TD(TD_BOOT), XXXXXXX, \
-  KC_CAPS, KC_F11,  KC_F4,   KC_F5,   KC_F6,   KC_SCRL,                     XXXXXXX, KC_RSFT,          KC_RCTL,       KC_LALT,    KC_RGUI,     XXXXXXX, \
+  KC_CAPS, KC_F11,  KC_F4,   KC_F5,   KC_F6,   KC_SCRL,                     KC_CAPS, KC_RSFT,          KC_RCTL,       KC_LALT,    KC_RGUI,     XXXXXXX, \
   XXXXXXX, KC_F10,  KC_F1,   KC_F2,   KC_F3,   KC_PAUS, KC_MUTE,   XXXXXXX, XXXXXXX, TD(TD_FUN),       TD(TD_MEDIA),  KC_RALT,    MO(_BUTTON), XXXXXXX, \
                   XXXXXXX, XXXXXXX, KC_APP, KC_SPC, KC_TAB,          _______, _______, _______, XXXXXXX, XXXXXXX \
 ),
 
 [_BUTTON] = LAYOUT( \
   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                       XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, \
-  XXXXXXX, KC_UNDO, S(KC_INS),  C(KC_INS), S(KC_INS), KC_AGIN,                       KC_AGIN, S(KC_INS), C(KC_INS), S(KC_INS),  KC_UNDO,  XXXXXXX, \
-  XXXXXXX, KC_LGUI, KC_LALT, KC_LCTL, KC_LSFT, XXXXXXX,                       XXXXXXX, KC_RSFT, KC_RCTL, KC_RALT, KC_RGUI, XXXXXXX, \
-  XXXXXXX, KC_UNDO, S(KC_INS),  C(KC_INS), S(KC_INS), KC_AGIN, KC_MUTE,     XXXXXXX, KC_AGIN, S(KC_INS), C(KC_INS), S(KC_INS),  KC_UNDO,  XXXXXXX, \
+  XXXXXXX, KC_UNDO, S(KC_DEL),  C(KC_INS), S(KC_INS), KC_AGIN,                       KC_AGIN, S(KC_INS), C(KC_INS), S(KC_DEL),  KC_UNDO,  XXXXXXX, \
+  XXXXXXX, KC_LGUI, KC_LALT, KC_LCTL, KC_LSFT, KC_CAPS,                       KC_CAPS, KC_RSFT, KC_RCTL, KC_RALT, KC_RGUI, XXXXXXX, \
+  XXXXXXX, KC_UNDO, S(KC_DEL),  C(KC_INS), S(KC_INS), KC_AGIN, KC_MUTE,     XXXXXXX, KC_AGIN, S(KC_INS), C(KC_INS), S(KC_DEL),  KC_UNDO,  XXXXXXX, \
               XXXXXXX, XXXXXXX, KC_BTN3, KC_BTN1, KC_BTN2,             KC_BTN2, KC_BTN1, KC_BTN3, XXXXXXX, XXXXXXX \
 ),
 
@@ -296,11 +296,11 @@ static void print_status_wide(void) {
             oled_write_ln_P(PSTR("OTHER"), true);
     }
 
+    oled_write_P(PSTR("\n  "), false);
+
     // Capslock indicator
     led_t led_usb_state = host_keyboard_led_state();
-    if (led_usb_state.caps_lock) {
-        oled_write_P(PSTR(" CAPS "), true);
-    }
+    oled_write_P(PSTR(" CAPS "), led_usb_state.caps_lock);
 }
 
 oled_rotation_t oled_init_user(oled_rotation_t rotation) {
