535e312024f75d3041c973f3ac7d41de016d47b0
Jonathan Hartshorn <j.hartshorn@outlook.com>
2024-06-22 01:20:23 +0100

    improve brackets layer

--- keyboards/ferris/keymaps/jph09/keymap.c
+++ keyboards/ferris/keymaps/jph09/keymap.c
@@ -80,6 +80,22 @@ void oneshot_mods_changed_user(uint8_t mods) {
 
 // KEYMAP STARTS HERE
 
+enum ferris_layers {
+    _BASE = 0,
+    _NUM,
+    _BRK,
+    _NAV,
+    _FUN,
+};
+
+// Aliases for ISO UK keys
+#define KC_GBP S(KC_3)      // Pound sign
+#define KC_EUR RALT(KC_4)   // Euro sign
+#define KC_NUDQ S(KC_2)     // Non-US double quote
+#define KC_NUAT S(KC_QUOT)  // Non-US at sign
+#define KC_NUPI S(KC_NUBS)  // Non-US pipe
+#define KC_NUTI S(KC_NUHS)  // Non-US tilde
+
 // Abbreviations for one-shots
 #define OSMLGUI OSM(MOD_LGUI)
 #define OSMLALT OSM(MOD_LALT)
@@ -91,30 +107,20 @@ void oneshot_mods_changed_user(uint8_t mods) {
 #define OSMRGUI OSM(MOD_RGUI)
 
 // Abbreviations for layer-taps
-#define LT2BSP LT(2,KC_BSPC)
-#define LT1ENT LT(1,KC_ENT)
-#define LT4QUO LT(4,KC_QUOT)
-
-// Extra aliases for ISO UK keys
-#define KC_GBP S(KC_3)      // Pound sign
-#define KC_EUR RALT(KC_4)   // Euro sign
-#define KC_NUDQ S(KC_2)     // Non-US Double Quote
-#define KC_NUAT S(KC_QUOT)  // Non-US At sign
+#define NUM_ENT LT(_NUM,KC_ENT)
+#define BRK_QUO LT(_BRK,KC_QUOT)
+#define NAV_BSP LT(_NAV,KC_BSPC)
 
 /* Override shifted keys */
 const key_override_t ko_scln = ko_make_basic(MOD_MASK_SHIFT, KC_COMM, KC_SCLN);
 const key_override_t ko_cln = ko_make_basic(MOD_MASK_SHIFT, KC_DOT, KC_COLN);
-const key_override_t ko_non_us_dquo = ko_make_basic(MOD_MASK_SHIFT, KC_QUOT, KC_NUDQ);
-const key_override_t ko_non_us_at = ko_make_basic(MOD_MASK_SHIFT, KC_2, KC_NUAT);
-const key_override_t ko_labk = ko_make_basic(MOD_MASK_SHIFT, KC_LPRN, KC_LABK);
-const key_override_t ko_rabk = ko_make_basic(MOD_MASK_SHIFT, KC_RPRN, KC_RABK);
+// const key_override_t ko_non_us_dquo = ko_make_basic(MOD_MASK_SHIFT, KC_QUOT, KC_NUDQ); // See case for BRK_QUO in process_record_user()
+// const key_override_t ko_non_us_at = ko_make_basic(MOD_MASK_SHIFT, KC_2, KC_NUAT);
 const key_override_t **key_overrides = (const key_override_t *[]){
     &ko_scln,
     &ko_cln,
-    &ko_non_us_dquo,
-    &ko_non_us_at,
-    &ko_labk,
-    &ko_rabk,
+    // &ko_non_us_dquo,
+    // &ko_non_us_at,
     NULL
 };
 
@@ -133,7 +139,7 @@ tap_dance_action_t tap_dance_actions[] = {
 #define TD_BOOT TD(_TD_BOOT)
 
 layer_state_t layer_state_set_user(layer_state_t state) {
-    state = update_tri_layer_state(state, 1, 2, 3);
+    state = update_tri_layer_state(state, _NUM, _NAV, _FUN);
     return state;
 }
 
@@ -254,6 +260,16 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
             }
             return true;
             break;
+        case BRK_QUO:
+            if (record->event.pressed && record->tap.count) {
+                // If shift is active and BRK_QUO is tapped
+                // then type double-quote instead of quote
+                if ((get_mods() | get_oneshot_mods()) & MOD_MASK_SHIFT) {
+                    tap_code16(KC_NUDQ);
+                    return false;
+                }
+            }
+            break;
     }
     return true;
 }
@@ -272,25 +288,33 @@ void post_process_record_user(uint16_t keycode, keyrecord_t *record) {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     /* COLEMAK Mod-DH base layer */
-    [0] = LAYOUT(                                                                             \
+    [_BASE] = LAYOUT(                                                                             \
     KC_Q,    KC_W,    KC_F,    KC_P,    KC_B,    KC_J,    KC_L,    KC_U,    KC_Y,    KC_MINS, \
     KC_A,    KC_R,    KC_S,    KC_T,    KC_G,    KC_M,    KC_N,    KC_E,    KC_I,    KC_O,    \
-    KC_Z,    KC_X,    KC_C,    KC_D,    KC_V,    KC_K,    KC_H,    KC_COMM, KC_DOT,  LT4QUO, \
-                               LT2BSP,  OSMLSFT, KC_SPC,  LT1ENT                              \
+    KC_Z,    KC_X,    KC_C,    KC_D,    KC_V,    KC_K,    KC_H,    KC_COMM, KC_DOT,  BRK_QUO, \
+                               NAV_BSP, OSMLSFT, KC_SPC,  NUM_ENT                             \
     ),
 
     /* Numbers and symbols layer
-    Missing: KC_PERC, KC_CIRC, KC_AMPR, S(KC_NUBS), S(KC_NUHS), S(KC_GRV), RALT(KC_GRV), KC_LCBR, KC_RCBR, KC_LABK, KC_RABK
+    Missing: S(KC_GRV), RALT(KC_GRV)
     */
-    [1] = LAYOUT(                                                                             \
+    [_NUM] = LAYOUT(                                                                             \
     KC_EUR,  KC_DLR,  KC_GBP,  KC_EXLM, KC_QUES, KC_EQL,  KC_ASTR, KC_SLSH, KC_PLUS, _______, \
     KC_7,    KC_5,    KC_3,    KC_1,    KC_9,    KC_8,    KC_0,    KC_2,    KC_4,    KC_6,    \
-    KC_NUBS, KC_NUHS, KC_NUAT, KC_LBRC, KC_LPRN, KC_RPRN, KC_RBRC, _______, _______, KC_GRV,  \
-                               _______, _______, _______, _______                             \
+    KC_NUBS, KC_NUPI, KC_NUHS, KC_NUAT, KC_NUTI, KC_CIRC, KC_AMPR, _______, _______, MO(_BRK),\
+                               _______, KC_PERC, _______, _______                             \
+    ),
+
+    /* Brackets layer */
+    [_BRK] = LAYOUT(                                                                             \
+    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
+    KC_GRV,  KC_LABK, KC_LBRC, KC_LPRN, _______, _______, KC_RPRN, KC_RBRC, KC_RABK, _______, \
+    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
+                               _______, KC_LCBR, KC_RCBR, _______                             \
     ),
 
     /* Navigation layer */
-    [2] = LAYOUT(                                                                             \
+    [_NAV] = LAYOUT(                                                                             \
     G(KC_L), C(KC_S), C(KC_F), KC_APP,  KC_ESC,  KC_DEL,  KC_SCRL, KC_PSCR, KC_PAUS, KC_INS,  \
     OSMLGUI, OSMLALT, OSMLCTL, OSMLSFT, KC_TAB,  KC_BSPC, KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT,\
     KC_UNDO, KC_CUT,  KC_COPY, KC_PSTE, KC_AGIN, KC_ENT,  KC_HOME, KC_PGDN, KC_PGUP, KC_END,  \
@@ -298,19 +322,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
 
     /* Function layer */
-    [3] = LAYOUT(                                                                             \
+    [_FUN] = LAYOUT(                                                                             \
     KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  \
     OSMLGUI, OSMLALT, OSMLCTL, OSMLSFT, KC_F11,  KC_F12,  OSMRSFT, OSMRCTL, OSMRALT, OSMRGUI, \
     CG_TOGG, RGB_VAD, RGB_VAI, KC_CAPS, TD_BOOT, XXXXXXX, KC_MUTE, KC_VOLD, KC_VOLU, KC_MPLY, \
                                _______, _______, _______, _______                             \
-    ),
-
-    /* Brackets layer */
-    [4] = LAYOUT(                                                                             \
-    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
-    KC_GRV,  KC_LABK, KC_LPRN, KC_LBRC, _______, _______, KC_RBRC, KC_RPRN, KC_RABK, _______, \
-    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
-                               _______, KC_LCBR, KC_RCBR, _______                             \
     ),
 
     /* Blank layer */
